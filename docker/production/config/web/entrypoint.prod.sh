e753e2b3d16fdc320ab112f6b28ec422791f27d3
Jesus Anaya <jesus.anaya.dev@gmail.com>
2020-08-12 14:41:41 -0700

    Removed nginx from docker.

--- docker/production/config/web/entrypoint.prod.sh
+++ docker/production/config/web/entrypoint.prod.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate --noinput
 python manage.py collectstatic --no-input
-gunicorn armandoanaya_blog.wsgi:application --bind 0.0.0.0:8000
+gunicorn armandoanaya_blog.wsgi:application --bind 0.0.0.0:8000 -w 4
