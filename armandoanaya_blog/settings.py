e22e673a1cf4ab94619779b09982e1396d54f8bb
Jesus Anaya <jesus.anaya.dev@gmail.com>
2020-08-12 23:44:10 -0700

    Added support for sentry logger.

--- armandoanaya_blog/settings.py
+++ armandoanaya_blog/settings.py
@@ -219,3 +219,16 @@ else:
 
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+# Install the Sentry SDK
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=os.environ.get('SENTRY_DNS'),
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
